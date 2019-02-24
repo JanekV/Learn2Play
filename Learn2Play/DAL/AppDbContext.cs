c98e0a7fd853d893004a7956a88f36057a97ba15
Janek <javalg@ttu.ee>
2019-02-24 16:13:57 +0200

    add erd again

---
+++ Learn2Play/DAL/AppDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using Domain;
+using Domain.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class AppDbContext: IdentityDbContext<AppUser, AppRole, int>
+    {
+        public DbSet<Folder> Folders { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Instrument> Instruments { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Style> Styles { get; set; }
+        public DbSet<Tab> Tabs { get; set; }
+        public DbSet<Video> Videos { get; set; }
+
+        public DbSet<ChordNote> ChordNotes { get; set; }
+        public DbSet<SongChord> SongChords { get; set; }
+        public DbSet<SongInFolder> SongInFolders { get; set; }
+        public DbSet<SongInstrument> SongInstruments { get; set; }
+        public DbSet<SongKey> SongKeys { get; set; }
+        public DbSet<SongStyle> SongStyles { get; set; }
+        public DbSet<TuningNote> TuningNotes { get; set; }
+        public DbSet<UserFolder> UserFolders { get; set; }
+        public DbSet<UserInstrument> UserInstruments { get; set; }
+        
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // disable cascade delete
+            foreach (var relationship in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+
+    }
+}
