6032ac7c02be5ba5be375381b3dcb0f9fed46179
Janek <javalg@ttu.ee>
2019-04-10 22:12:25 +0300

    added 3 controllers for notes, chords and chordnotes

---
+++ Learn2PlayClient/src/chordnotes/delete.ts
@@ -0,0 +1,47 @@
+import { LogManager, View } from "aurelia-framework";
+import { RouteConfig, NavigationInstruction } from "aurelia-router";
+
+export var log = LogManager.getLogger('ChordNotes.Delete');
+
+export class Delete{
+  constructor(){
+    log.debug('constructor');
+  }
+
+  // =============== View LifeCycle events ================
+  created(owningView: View, myView: View) {
+    log.debug('created');
+  }
+
+  bind(bindingContext: Object,overrideContext: Object) {
+    log.debug('bind');
+  }
+
+  attached() {
+    log.debug('attatched');
+  }
+
+  detached() {
+    log.debug('detatched');
+  }
+
+  unbind() {
+    log.debug('unbind');
+  }
+
+  // =============== Router Events =======================
+  canActivate(params: any, routeConfig: RouteConfig, navigationInstruction: NavigationInstruction) {
+    log.debug('canActivate');
+  }
+  activate(params: any, routeConfig: RouteConfig, navigationInstruction: NavigationInstruction) {
+    log.debug('activate');
+  }
+
+  canDeactivate() {
+    log.debug('canDeactivate');
+  }
+
+  deactivate() {
+    log.debug('deactivate');
+  }
+}
--- Learn2PlayClient/src/main-router.ts
+++ Learn2PlayClient/src/main-router.ts
@@ -16,7 +16,35 @@ export class MainRouter{
     this.router = router;
     config.title = "Learn2Play App - Aurelia";
     config.map([
-      {route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('home'), nav: true, title: 'Home'}
+      {
+        route: ['', 'home'],
+        name: 'home',
+        moduleId: PLATFORM.moduleName('home'),
+        nav: true, 
+        title: 'Home'
+      },
+      //Notes controller routes
+      {route: ['notes' ,'notes/index'], name: 'notes' + 'Index', moduleId: PLATFORM.moduleName('notes/index'), nav: true, title: 'Notes'},
+      {route: 'notes/create', name: 'notes' + 'Create', moduleId: PLATFORM.moduleName('notes/create'), nav: false, title: 'Notes Create'},
+      {route: 'notes/edit/:id', name: 'notes' + 'Edit', moduleId: PLATFORM.moduleName('notes/edit'), nav: false, title: 'Notes Edit'},
+      {route: 'notes/details/:id', name: 'notes' + 'Details', moduleId: PLATFORM.moduleName('notes/details'), nav: false, title: 'Notes Details'},
+      {route: 'notes/delete/:id', name: 'notes' + 'Delete', moduleId: PLATFORM.moduleName('notes/delete'), nav: false, title: 'Notes Delete'},
+
+      //Chords controller routes
+      {route: ['chords' ,'chords/index'], name: 'chords' + 'Index', moduleId: PLATFORM.moduleName('chords/index'), nav: true, title: 'Chords'},
+      {route: 'chords/create', name: 'chords' + 'Create', moduleId: PLATFORM.moduleName('chords/create'), nav: false, title: 'Chords Create'},
+      {route: 'chords/edit/:id', name: 'chords' + 'Edit', moduleId: PLATFORM.moduleName('chords/edit'), nav: false, title: 'Chords Edit'},
+      {route: 'chords/details/:id', name: 'chords' + 'Details', moduleId: PLATFORM.moduleName('chords/details'), nav: false, title: 'Chords Details'},
+      {route: 'chords/delete/:id', name: 'chords' + 'Delete', moduleId: PLATFORM.moduleName('chords/delete'), nav: false, title: 'Chords Delete'},
+
+      //ChordNotes controller routes
+      {route: ['chordnotes' ,'chordnotes/index'], name: 'chordnotes' + 'Index', moduleId: PLATFORM.moduleName('chordnotes/index'), nav: true, title: 'ChordNotes'},
+      {route: 'chordnotes/create', name: 'chordnotes' + 'Create', moduleId: PLATFORM.moduleName('chordnotes/create'), nav: false, title: 'ChordNotes Create'},
+      {route: 'chordnotes/edit/:id', name: 'chordnotes' + 'Edit', moduleId: PLATFORM.moduleName('chordnotes/edit'), nav: false, title: 'ChordNotes Edit'},
+      {route: 'chordnotes/details/:id', name: 'chordnotes' + 'Details', moduleId: PLATFORM.moduleName('chordnotes/details'), nav: false, title: 'ChordNotes Details'},
+      {route: 'chordnotes/delete/:id', name: 'chordnotes' + 'Delete', moduleId: PLATFORM.moduleName('chordnotes/delete'), nav: false, title: 'ChordNotes Delete'},
+
+      
     ]);
   }
 }
